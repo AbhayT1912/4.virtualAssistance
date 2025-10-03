e8890921f56dcc9040d14a2425451843e45b8dda
Abhay Thakur <156583270+AbhayT1912@users.noreply.github.com>
2025-10-03 14:12:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ import geminiResponse from './gemini.js';
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://virtualassistance-gddq.onrender.com',
   credentials: true,
 }))
 const PORT = process.env.PORT || 5000;
